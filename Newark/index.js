004b29a7db28143ea950845838fa4b45fa85f425
Alina <pshenolina@yahoo.com>
2024-10-03 14:15:47 +0300

    create new creative

---
+++ Newark/index.js
@@ -0,0 +1,30 @@
+
+// const gallery = document.querySelector(".gallery");
+// const arrowsRight = document.querySelectorAll(".right");
+// let count = 0;
+
+// const shiftGalleryRight = () => {
+//   count++;
+//   gallery.style.transform = `translateX(-${count * 100}vw)`;
+//   // размер должен быть одинаковым тут и в цсс строка 14
+//   console.log(count);
+// };
+
+// [...arrowsRight].forEach((el) =>
+//   el.addEventListener("click", () => {
+//     shiftGalleryRight();
+//   })
+// );
+// const arrowsLeft = document.querySelectorAll(".left");
+
+// const shiftGalleryLeft = () => {
+//   count--;
+//   gallery.style.transform = `translateX(-${count * 100}vw)`;
+//   console.log(count);
+// };
+
+// [...arrowsLeft].forEach((el) =>
+//   el.addEventListener("click", () => {
+//     shiftGalleryLeft();
+//   })
+// );
